2b8db469ccf13abf94aca35225be3c409647b10a
Jannik Lorenz <dev@janniklorenz.de>
2015-08-18 17:03:20 +0200

    basic ace editor integration (still buggy)

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,46 +13,27 @@ window.onload = function() {
 	});
 
 
-	var pad = document.getElementById('pad');
+
+	var pad = ace.edit("pad");
+	pad.setTheme("ace/theme/solarized_dark");
+
 	var markdownArea = document.getElementById('markdown');
 
 
-	// make the tab act like a tab
-	pad.addEventListener('keydown',function(e) {
-		if(e.keyCode === 9) { // tab was pressed
-			// get caret position/selection
-			var start = this.selectionStart;
-			var end = this.selectionEnd;
 
-			var target = e.target;
-			var value = target.value;
 
-			// set textarea value to: text before caret + tab + text after caret
-			target.value = value.substring(0, start)
-							+ "\t"
-							+ value.substring(end);
-
-			// put caret at right position again (add one for the tab)
-			this.selectionStart = this.selectionEnd = start + 1;
-
-			// prevent the focus lose
-			e.preventDefault();
-		}
-	});
 
 	var previousMarkdownValue;
 
 	// convert text area to markdown html
 	var convertTextAreaToMarkdown = function(){
-		var markdownText = pad.value;
+		var markdownText = pad.getValue();
 		previousMarkdownValue = markdownText;
 
 		html = marked(markdownText);
-
-		// html = converter.makeHtml(markdownText);
-
-
 		markdownArea.innerHTML = html;
+
+		$("table").addClass("table table-striped")
 	};
 
 	var didChangeOccur = function(){
@@ -70,20 +51,21 @@ window.onload = function() {
 	}, 1000);
 
 	// convert textarea on input change
-	pad.addEventListener('input', convertTextAreaToMarkdown);
+	// pad.addEventListener('input', convertTextAreaToMarkdown);
+	// pad.on('change', convertTextAreaToMarkdown);
+
 
 	// ignore if on home page
 	if(document.location.pathname.length > 1){
 		// implement share js
 		var documentName = document.location.pathname.substring(1);
 		sharejs.open(documentName, 'text', function(error, doc) {
-			doc.attach_textarea(pad);
+			doc.attach_ace(pad);
 			convertTextAreaToMarkdown();
 		});
 	}
 
 	// convert on page load
 	convertTextAreaToMarkdown();
-
 
 };
